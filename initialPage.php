3308ad92222f9b028b8722b185943881e53eaf80
muntasirrifat23 <www.muntasirrifat23@gmail.com>
2026-07-28 11:51:44 +0600

    initial page grid 4

--- initialPage.php
+++ initialPage.php
@@ -262,7 +262,7 @@ mysqli_close($db);
 
         .btn-grid {
             display: grid;
-            grid-template-columns: repeat(6, 1fr);
+            grid-template-columns: repeat(4, 1fr);
             gap: 12px;
             margin: 24px 0;
         }
