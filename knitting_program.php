49ef5029890d9b342a16fb462c5fc55b113c390f
muntasirrifat23 <www.muntasirrifat23@gmail.com>
2026-08-17 16:10:24 +0600

    changed style 08

--- knitting_program.php
+++ knitting_program.php
@@ -599,7 +599,7 @@
     <!-- SEARCH -->
     <div class="card-unified">
         <div class="search-controls">
-            <input type="text" id="bookingInput" placeholder="Please enter SUB_TID or PO Number">
+            <input type="text" id="bookingInput" placeholder="Please Enter PO Number">
             <button class="btn-search" id="searchBtn"><i class="fa-solid fa-magnifying-glass me-1"></i>Search</button>
             <button class="btn-clear" id="clearBtn"><i class="fa-solid fa-rotate-left me-1"></i>Clear</button>
         </div>
